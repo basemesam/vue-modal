4ad4d6d2cd9792a870cdbd2fec596204aaf901ad
bassem essam <bassem.essam@forus.cc>
2023-01-30 13:28:36 +0200

    add project files

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+
+  <div style="margin-bottom: 20px">
+    <input type="text" ref="name" />
+    <button @click="handleClick">Click</button>
+  </div>
+
+  <teleport to=".modals" v-if="showModal">
+    <Modal @close="toggleModal" theme="sale">
+      <template v-slot:links>
+        <a href="#">Click 1</a>
+        <a href="#">Click 2</a>
+      </template>
+
+      <h1>Content Header</h1>
+      <p>Lorem ipsum</p>
+    </Modal>
+  </teleport>
+
+  <button @click="toggleModal">Toggle Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "Hello vue :)",
+      showModal: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app,
+.modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Modal.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot></slot>
+
+      <div class="action">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Modal",
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 200px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  left: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  font-weight: 500;
+  color: #f00;
+}
+.modal p {
+  font-style: normal;
+}
+.modal.sale {
+  background-color: #f00;
+  color: white;
+}
+
+.modal.sale h1 {
+  color: white;
+}
+</style>
